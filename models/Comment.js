b88337962101b6dc29e00b2d24f0e7143ea4dea5
toddrgarner <110719370+toddrgarner@users.noreply.github.com>
2022-10-11 16:36:59 -0700

    added user.js, index, and user.js

---
+++ models/Comment.js
@@ -0,0 +1,44 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model{}
+
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+       },
+        comment: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+       
+        post_id: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+            model: 'post',
+                key: 'id',
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id',
+            },
+        },
+    },
+
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'comment',
+      }
+
+)
+
+module.exports = Comment;
---
+++ models/Index.js
@@ -0,0 +1,28 @@
+// import models
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
+
+// User Relationship
+User.hasMany(Post,{
+    foreignKey: 'user_id'
+});
+
+User.hasMany(Comment,{
+    foreignKey: 'user_id'
+});
+
+// Post Relationships
+Post.belongsTo(User,{
+    foreignKey: "user_id"
+});
+
+// Comment Relationships
+Comment.belongsTo(Post, {
+    foreignKey: "post_id"
+});
+
+Comment.belongsTo(User, {
+    foreignKey: "user_id"
+});
+
