a32e49267e401d349cffdb392687a236705e8cb3
toddrgarner <110719370+toddrgarner@users.noreply.github.com>
2022-10-15 04:06:35 -0700

    added index.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 
-const userRoutes = require('/user-routes.js');
-const postRoutes = require('/post-routes');
-const commentRoutes = require('/comment-routes');
+const apiRoutes = require('./api/');
+const homeRoutes = require('./home-routes.js');
+const dashboardRoutes = require('./dashboard-routes.js');
 
-router.use('/users', userRoutes);
-router.use('/posts', postRoutes);
-router.use('/comments', commentRoutes);
+router.use('/dashboard', dashboardRoutes);
+router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
 
 module.exports = router;
