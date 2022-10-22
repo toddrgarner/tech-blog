3d2c07994116e52a146b3e80d8f8974369727c57
toddrgarner <110719370+toddrgarner@users.noreply.github.com>
2022-10-21 20:29:27 -0700

    updated index.js

--- models/Index.js
+++ models/Index.js
@@ -1,34 +1,35 @@
 // import models
+const POst = require('./Post');
 const User = require('./User');
-const Post = require('./Post');
 const Comment = require('./Comment');
 
-// User Relationship
-User.hasMany(Post,{
+// create associations
+User.hasMany(Post, {
     foreignKey: 'user_id'
 });
 
-User.hasMany(Comment,{
-    foreignKey: 'user_id'
-});
-
-// Post Relationships
-Post.belongsTo(User,{
-    foreignKey: "user_id"
-});
-
-Post.belongsTo(Comment,{
-    foreignKey: "post_id"
-});
-
-
-// Comment Relationships
-Comment.belongsTo(Post, {
-    foreignKey: "post_id"
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
 });
 
 Comment.belongsTo(User, {
-    foreignKey: "user_id"
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id',
+    onDelete: 'SET NULL'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
 });
 
 module.exports = { User, Post, Comment };
